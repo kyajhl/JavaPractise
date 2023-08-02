6b7d2bc4c538b3a157f304730d60a8584ffb3d31
kyajhl <136273344@qq.com>
2023-08-02 11:02:25 +0800

    8.2

--- src/day07/JavaDemo.java
+++ src/day07/JavaDemo.java
@@ -1,5 +1,11 @@
 package day07;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author KeFan
  * @date 2023/6/12
@@ -21,5 +27,19 @@ public class JavaDemo {
 //        new R().printOk();
 
         R.getR().printOk();
+
+        User user1 = new User("aaa", 10);
+        User user2 = new User("bbb", 20);
+        User user3 = new User("ccc", 30);
+
+        ArrayList<User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+
+        List<User> list1 = list.stream().peek(user -> user.setName("qwe")).collect(Collectors.toList());
+        List<String> list2 = list.stream().map(User::getName).collect(Collectors.toList());
+        System.out.println(list1);
+        System.out.println(list2);
     }
 }
